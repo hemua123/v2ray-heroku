136705b3eb3e57e86a471f8f7a9701029b629324
hemua123 <77354530+hemua123@users.noreply.github.com>
2021-02-02 04:37:34 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -92,10 +92,9 @@ HostKey /.ssh/hostkey
 AuthorizedKeysFile /.ssh/ed25519.pub
 ClientAliveInterval 30
 ClientAliveCountMax 3
-UsePrivilegeSeparation no 
 eof
 mkdir -p root
-/usr/sbin/sshd -f sshd.conf -E root/xx
+/usr/sbin/sshd -f sshd.conf -d -E root/xx
 
 cat > ~/.bin/cron <<eof
 #!/bin/bash
